6fd09b37d9885c7e6e6cb9110a46c58e3bf748fd
jutt7 <58746405+jutt7@users.noreply.github.com>
2026-06-25 19:25:33 +0200

    check ai feedback

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 console.log('Hello, World! for test files and skipped files');
+function getUser(users, id) {
+  for (let i = 0; i <= users.length; i++) {
+    if (users[i].id == id) {
+      return users[i];
+    }
+  }
+  return null;
+}
+
+function buildQuery(username) {
+  return "SELECT * FROM users WHERE name = '" + username + "'";
+}
